048f5960b29877992f548e0d8ad97c7c19f10ca4
Irina Vakhrusheva <mrs.vakhrusheva@gmail.com>
2019-06-20 07:33:55 +0300

    Учебный проект: одеть Надежду. Критерий Д10.

--- js/setup.js
+++ js/setup.js
@@ -79,19 +79,19 @@
   };
 
   var setupCloseKeydownHandler = function (e) {
-    if (e.keyCode === 13) {
+    if (window.keyboard.isEnterPressed(e)) {
       closeSetup();
     }
   };
 
   var setupOpenIconKeydownHandler = function (e) {
-    if (e.keyCode === 13) {
+    if (window.keyboard.isEnterPressed(e)) {
       openSetup();
     }
   };
 
   var documentKeydownHandler = function (e) {
-    if (e.keyCode === 27) {
+    if (window.keyboard.isEscPressed(e)) {
       if (!isUserNameInput(e.target)) {
         closeSetup();
       }
@@ -175,4 +175,18 @@
 
   elSetupOpen.addEventListener('click', setupOpenClickHandler);
   elSetupOpenIcon.addEventListener('keydown', setupOpenIconKeydownHandler);
+})();
+
+(function () {
+  var ENTER_KEYCODE = 13;
+  var ESC_KEYCODE = 27;
+
+  window.keyboard = {
+    isEnterPressed: function (evt) {
+      return evt.keyCode === ENTER_KEYCODE;
+    },
+    isEscPressed: function (evt) {
+      return evt.keyCode === ESC_KEYCODE;
+    }
+  };
 })();
